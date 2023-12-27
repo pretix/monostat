6b97d47c3fa07bbe96c3d9937e00849405d5ba07
Raphael Michel <mail@raphaelmichel.de>
2023-12-27 13:30:09 +0100

    Add relative dates

--- monostat/static/public/main.js
+++ monostat/static/public/main.js
@@ -2,9 +2,9 @@ var DateTime = luxon.DateTime;
 document.addEventListener("DOMContentLoaded", function (event) {
     document.querySelectorAll("time[datetime]").forEach((el) => {
         const datetime = el.attributes["datetime"].value;
-        const dt = DateTime.fromISO(datetime);
-        const date = dt.setLocale("en").toLocaleString(DateTime.DATE_MED);
-        const time = dt.setLocale("en").toLocaleString(DateTime.TIME_24_WITH_SHORT_OFFSET);
-        el.innerHTML = date + " " + time;
+        const dt = DateTime.fromISO(datetime).setLocale("en");
+        const date = dt.toLocaleString(DateTime.DATE_MED);
+        const time = dt.toLocaleString(DateTime.TIME_24_WITH_SHORT_OFFSET);
+        el.innerHTML = date + " " + time + " (" + dt.toRelative() + ")";
     })
 });
